7288aa005cc93f38c26bcd0a155c52efe36753a0
Elwin BASSAGET <elwin.bassaget@data-tricks.fr>
2024-06-20 11:12:55 +0200

    Remove dist directory from repository

--- dist/src/types.js
+++
@@ -1,5 +0,0 @@
-export var Speciality;
-(function (Speciality) {
-    Speciality["Ophtalmologist"] = "OPHTALMOLOGIST";
-    Speciality["Psychologist"] = "PSYCHOLOGIST";
-})(Speciality || (Speciality = {}));
